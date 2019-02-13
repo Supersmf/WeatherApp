9bef31c8ed006f1a5a39e8266b07c1760018e659
Aleh Haidukevich <Aleh.Haidukevich@icx.local>
2019-02-13 13:53:45 +0300

    add search by city

---
+++ src/addData.js
@@ -0,0 +1,24 @@
+let fillWeatherData = (data, element) => {
+
+    let container = document.createElement('div');
+    let location = document.createElement('h3');
+    let temp = document.createElement('p');
+    let weather = document.createElement('p');
+    let pressure = document.createElement('p');
+
+    location.innerHTML = `${data.name},${data.sys.country}`;
+    temp.innerHTML = `Temperature: ${data.main.temp} F`;
+    weather.innerHTML = `Weather: ${data.weather[0].description}`;
+    pressure.innerHTML = `Pressure: ${data.main.pressure}`;
+
+    container.appendChild(location);
+    container.appendChild(temp);
+    container.appendChild(weather);
+    container.appendChild(pressure);
+
+    element.appendChild(container);
+}
+
+export {
+    fillWeatherData
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import getData from './getData';
-import getLocation from './getLocation';
+import { fillWeatherData } from './addData';
 import './style/style.scss';
 
 const buttonSearch = document.querySelector('.btnSearch');
 const inputSearch = document.querySelector('.inputSearch');
+const localWeather = document.querySelector('.localWeather');
+const currentWeather = document.querySelector('.currentWeather');
 
-const url = 'https://samples.openweathermap.org/data/2.5/weather?';
+const url = 'http://api.openweathermap.org/data/2.5/weather?';
+const ipUrl = "https://ipinfo.io/json";
 const corst = 'https://cors.io/?';
 const appid = '361769565b95b84393f71027df69289e';
 
 window.addEventListener('load', () => {
-    getLocation(corst + url, appid);
+
+    fetch(corst + ipUrl).then((response) => response.json())
+        .then(({city}) => getData(corst + url, appid, city))
+        .then(data => fillWeatherData(data, localWeather));
 })
 
 
-/* buttonSearch.addEventListener('click', (e) => {
-    let input = inputSearch.value;
-    getData().then(res => console.log(res));;
-}) */
+buttonSearch.addEventListener('click', (e) => {
+    let city = inputSearch.value;
+    inputSearch.value = '';
+    getData(corst + url, appid, city)
+        .then(data => fillWeatherData(data, currentWeather));;
+})
